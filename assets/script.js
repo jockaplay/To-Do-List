35656a8cadc9cb91482e40b43a5e48b3031985d3
jockaplay <jocksonmatheus@gmail.com>
2023-03-16 14:48:42 -0300

    Implement, check buttom

--- assets/script.js
+++ assets/script.js
@@ -32,13 +32,22 @@ const showValues = () => {
     let list = document.getElementById('to-do-list')
     list.innerHTML = ''
     for(let i = 0; i < values.length; i++){
-        list.innerHTML += `<div class="task">
+        list.innerHTML += `<div class="task ${values[i]["checked"] ? "checked": ""}">
             <p>${values[i]["name"][0]}</p>
             <div>
+            <button onclick='checkItem("${values[i]["name"]}")'><img src="assets/src/img/task.svg" alt="check"></button>
             <button onclick='removeItem("${values[i]["name"]}")'><img src="assets/src/img/trash.svg" alt="delete"></button>
             </div>
         </div>`
     }
+}
+
+const checkItem = (data) => {
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || '[]')
+    let index = values.findIndex(x => x.name == data)
+    values.splice(index, 1, {name: values[index]["name"], checked: !values[index]["checked"]})
+    localStorage.setItem(localStorageKey, JSON.stringify(values))
+    showValues()
 }
 
 const removeItem = (data) => {
